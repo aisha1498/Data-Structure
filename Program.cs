2ffbd964d1bff5bc7a329b3c38093ffbae342cb6
aisha1498 <95531968+aisha1498@users.noreply.github.com>
2022-02-14 09:05:28 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,9 @@
-﻿namespace DataStrucutre
-{
-    class program
-    {
-        static void Main(string[] args)
-        {
-            //LinkedList<int> list = new LinkedList<int>
-            //
-            //
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructures;
+
+Console.WriteLine("Hello World!");
+LinkedList list = new LinkedList();
+list.Add(56);
+list.Add(30);
+list.Add(70);
+Console.ReadKey();
