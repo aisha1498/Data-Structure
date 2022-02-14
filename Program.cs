e198ad434e8f97d2375bfc65cd7999c4c1babb6f
aisha1498 <95531968+aisha1498@users.noreply.github.com>
2022-02-14 09:30:25 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@ using DataStructures;
 
 Console.WriteLine("Hello World!");
 LinkedList list = new LinkedList();
-list.Add(56);
-list.Add(30);
 list.Add(70);
+list.Add(30);
+list.Add(56);
+list.InsertAtParticularPosition(2, 30);
 Console.ReadKey();
